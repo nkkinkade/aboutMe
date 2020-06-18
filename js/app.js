2837dd263092a453906d1b5a8acb791b85ce065b
Nathan Kinkade <nkkinkade@gmail.com>
2020-06-17 20:38:47 -0500

    add score counter

--- js/app.js
+++ js/app.js
@@ -23,7 +23,9 @@ if (hair === 'yes' || hair === 'y') {
 } else if (hair === 'no' || hair === 'n') {
 //   console.log('That\'s correct! Nathan has been shaving his head for over 10 years now!');
   alert('That\'s correct! Nathan has been shaving his head for over 10 years now!');
+  userPoints += 1;
 }
+// console.log('This is the user point total: ', userPoints);
 
 var degree = prompt('Next question, ' + userName + '. Does Nathan have more than one post-high school degree?').toLowerCase().trim();
 while (degree !== 'yes' && degree !== 'no' && degree !== 'y' && degree !== 'n') {
@@ -32,10 +34,12 @@ while (degree !== 'yes' && degree !== 'no' && degree !== 'y' && degree !== 'n') 
 if (degree === 'yes' || degree === 'y') {
 //   console.log('Correct! Nathan has 2 Associate degrees and a Bachelor degree.');
   alert('Correct! Nathan has 2 Associate degrees and a Bachelor degree.');
+  userPoints += 1;
 } else if (degree === 'no' || degree === 'n') {
 //   console.log('Wrong! Nathan has 2 Associate degrees and a Bachelor degree.');
   alert('Wrong! Nathan has 2 Associate degrees and a Bachelor degree.');
 }
+// console.log('This is the user point total: ', userPoints);
 
 var kids = prompt('Third question. Does Nathan have any children?').toLowerCase().trim();
 while (kids !== 'yes' && kids !== 'no' && kids !== 'y' && kids !== 'n') {
@@ -44,10 +48,12 @@ while (kids !== 'yes' && kids !== 'no' && kids !== 'y' && kids !== 'n') {
 if (kids === 'yes' || kids === 'y') {
 //   console.log('Correct! Nathan has 1 son.');
   alert('Correct! Nathan has 1 son.');
+  userPoints += 1;
 } else if (kids === 'no' || kids === 'n') {
 //   console.log('Wrong, sorry! Nathan has 1 son.');
   alert('Wrong, sorry! Nathan has 1 son.');
 }
+// console.log('This is the user point total: ', userPoints);
 
 var pets = prompt('Are you an animal lover, ' + userName + '? How about Nathan, do you think he has any pets?').toLowerCase().trim();
 while (pets !== 'yes' && pets !== 'no' && pets !== 'y' && pets !== 'n') {
@@ -56,10 +62,12 @@ while (pets !== 'yes' && pets !== 'no' && pets !== 'y' && pets !== 'n') {
 if (pets === 'yes' || pets === 'y') {
 //   console.log('Absolutely! Nathan has 1 puppy name Beanie!');
   alert('Absolutely! Nathan has 1 puppy name Beanie!');
+  userPoints += 1;
 } else if (pets === 'no' || pets === 'n') {
 //   console.log('Wrong. Nathan is definitely an animal person. He has 1 puppy name Beanie!');
   alert('Wrong. Nathan is definitely an animal person. He has 1 puppy name Beanie!');
 }
+// console.log('This is the user point total: ', userPoints);
 
 var travel = prompt('Fifth one, ' + userName + '! Has Nathan ever traveled across the ocean?').toLowerCase().trim();
 while (travel !== 'yes' && travel !== 'no' && travel !== 'y' && travel !== 'n') {
@@ -71,7 +79,9 @@ if (travel === 'yes' || travel === 'y') {
 } else if (travel === 'no' || travel === 'n') {
 //   console.log('That\'s correct! Nathan has been to Canada and Mexico, but has yet to leave the continent.');
   alert('That\'s correct! Nathan has been to Canada and Mexico, but has yet to leave the continent.');
+  userPoints += 1;
 }
+// console.log('This is the user point total: ', userPoints);
 
 // The following code was borrowed from Craig during Lab discussion
 var opportunities = 4;
@@ -86,6 +96,7 @@ while (opportunities > 0) {
 
   if (userChoice === answer) {
     alert('Correct! Great guessing!');
+    userPoints += 1;
     opportunities = 0;
     break;
   } else {
@@ -100,5 +111,8 @@ while (opportunities > 0) {
   opportunities = opportunities - 1;
   console.log('guessCount is ' + opportunities);
 }
-
 alert('The answer was 5!');
+
+
+
+console.log('This is the user point total: ', userPoints);
