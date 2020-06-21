2ab1a8a07e1d0fd06ed85868f4c791ea831f8aed
Nathan Kinkade <nkkinkade@gmail.com>
2020-06-21 18:16:14 -0500

    cleaned up commented out code plus add top 10 movies

--- js/app.js
+++ js/app.js
@@ -19,14 +19,12 @@ function question1() {
     hair = prompt('Please use yes/no or y/n. Does Nathan have long hair?').toLowerCase().trim();
   }
   if (hair === 'yes' || hair === 'y') {
-    //   console.log('Nope! Nathan has been shaving his head for over 10 years now!');
     alert('Nope! Nathan has been shaving his head for over 10 years now!');
   } else if (hair === 'no' || hair === 'n') {
-    //   console.log('That\'s correct! Nathan has been shaving his head for over 10 years now!');
     alert('That\'s correct! Nathan has been shaving his head for over 10 years now!');
     userPoints += 1;
   }
-// console.log('This is the user point total: ', userPoints);
+
 } question1();
 
 function question2() {
@@ -35,68 +33,52 @@ function question2() {
     degree = prompt('Please use yes/no or y/n. Does Nathan have more than one post-high school degree?').toLowerCase().trim();
   }
   if (degree === 'yes' || degree === 'y') {
-    //   console.log('Correct! Nathan has 2 Associate degrees and a Bachelor degree.');
     alert('Correct! Nathan has 2 Associate degrees and a Bachelor degree.');
     userPoints += 1;
   } else if (degree === 'no' || degree === 'n') {
-    //   console.log('Wrong! Nathan has 2 Associate degrees and a Bachelor degree.');
     alert('Wrong! Nathan has 2 Associate degrees and a Bachelor degree.');
   }
-// console.log('This is the user point total: ', userPoints);
 } question2();
-//
 function question3() {
   var kids = prompt('Third question. Does Nathan have any children?').toLowerCase().trim();
   while (kids !== 'yes' && kids !== 'no' && kids !== 'y' && kids !== 'n') {
     kids = prompt('How many times must we go over this, ' + userName + '? Please use yes/no or y/n. Does Nathan have any children?').toLowerCase().trim();
   }
   if (kids === 'yes' || kids === 'y') {
-    //   console.log('Correct! Nathan has 1 son.');
     alert('Correct! Nathan has 1 son.');
     userPoints += 1;
   } else if (kids === 'no' || kids === 'n') {
-    //   console.log('Wrong, sorry! Nathan has 1 son.');
     alert('Wrong, sorry! Nathan has 1 son.');
   }
-// console.log('This is the user point total: ', userPoints);
 } question3();
-//
 function question4() {
   var pets = prompt('Are you an animal lover, ' + userName + '? How about Nathan, do you think he has any pets?').toLowerCase().trim();
   while (pets !== 'yes' && pets !== 'no' && pets !== 'y' && pets !== 'n') {
     pets = prompt('Use yes/no or y/n, ' + userName + '. I don\'t what to have to tell you again. Does Nathan have any pets?').toLowerCase().trim();
   }
   if (pets === 'yes' || pets === 'y') {
-    //   console.log('Absolutely! Nathan has 1 puppy name Beanie!');
     alert('Absolutely! Nathan has 1 puppy name Beanie!');
     userPoints += 1;
   } else if (pets === 'no' || pets === 'n') {
-    //   console.log('Wrong. Nathan is definitely an animal person. He has 1 puppy name Beanie!');
     alert('Wrong. Nathan is definitely an animal person. He has 1 puppy name Beanie!');
   }
-// console.log('This is the user point total: ', userPoints);
+
 } question4();
-//
+
 function question5() {
   var travel = prompt('Fifth one, ' + userName + '! Has Nathan ever traveled across the ocean?').toLowerCase().trim();
   while (travel !== 'yes' && travel !== 'no' && travel !== 'y' && travel !== 'n') {
     travel = prompt('Yes/no or y/n only, ' + userName + '. I\'m disapointed in you. Has Nathan ever traveled across the ocean?').toLowerCase().trim();
   }
   if (travel === 'yes' || travel === 'y') {
-    //   console.log('Nope! Nathan has been to Canada and Mexico, but has yet to leave the continent.');
     alert('Nope! Nathan has been to Canada and Mexico, but has yet to leave the continent.');
   } else if (travel === 'no' || travel === 'n') {
-    //   console.log('That\'s correct! Nathan has been to Canada and Mexico, but has yet to leave the continent.');
     alert('That\'s correct! Nathan has been to Canada and Mexico, but has yet to leave the continent.');
     userPoints += 1;
   }
-// console.log('This is the user point total: ', userPoints);
+
 } question5();
 //  The following code was borrowed from Craig during Lab discussion
-
-// Add a 6th question to the guessing game that takes in a numeric input by prompting the user to guess a number.
-// It should give the user exactly four opportunities to get the correct answer.
-//
 function question6() {
   var opportunities = 4;
   var answer = 5;
@@ -114,7 +96,6 @@ function question6() {
       opportunities = 0;
       break;
     }
-    // Indicates through an alert if the guess is “too high” or “too low”.
     else {
       if (userChoice < answer){
         alert('Too Low!');
@@ -126,21 +107,15 @@ function question6() {
     opportunities--;
     console.log('guessCount is ' + opportunities);
   }
-  // After all attempts have been exhausted, tell the user the correct answer. Consider using a loop of some sort.
   alert('The answer was ' + answer + '!');
 } question6();
-
-
-// Add a 7th question that has multiple possible correct answers that are stored in an array.
 
 function question7() {
   var favoriteFood = ['pizza', 'tacos', 'steak', 'potatoes'];
   console.log(favoriteFood);
 
-  // Give the user 6 attempts to guess the correct answer.
   var opportunitiesTwo = 6;
 
-  // Consider using a loop of some sort for this question.
   while (opportunitiesTwo > 0) {
     var guess = prompt(userName + ', what is one of Nathan\'s favorite foods? You have ' + opportunitiesTwo + ' guesses left.').toLowerCase().trim();
     var correct = false;
@@ -164,11 +139,7 @@ function question7() {
     opportunitiesTwo--;
   }
 
-  // The guesses will end once the user guesses a correct answer or they run out of attempts.
-  // Display all the possible correct answers to the user.
   alert('My favorite foods are ' + favoriteFood + '. Thanks for playing!');
 } question7();
 alert('You got ' + userPoints + ' out of 7 points correct.');
 console.log('This is the user point total: ', userPoints);
-
-
